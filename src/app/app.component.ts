cc81c19f993feb3d72a6474354e7fabd561283ce
argeulimbo <argeusousaptbr@gmail.com>
2025-06-02 15:05:49 -0300

    update: change version typescript to 5.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: 'app.component.html'
+  imports: [RouterOutlet, MatButtonModule],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'helpdesk';
