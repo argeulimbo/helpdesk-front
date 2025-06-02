ff3f5ff8e3808a6303063dac41980c8e1fc14772
argeulimbo <argeusousaptbr@gmail.com>
2025-06-02 15:42:35 -0300

    add: adicionado imports de forms e reactiveForms do Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+// Angular 12+ 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
@@ -21,6 +25,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatRippleModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -51,11 +56,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
             MatRadioModule,
             MatRippleModule,
             MatSelectModule,
+            MatSidenavModule,
             MatSlideToggleModule,
             MatSliderModule,
             MatSnackBarModule,
             MatTabsModule,
-            MatToolbarModule
+            MatToolbarModule,
+            FormsModule,
+            ReactiveFormsModule
           ],
 
   templateUrl: './app.component.html',
