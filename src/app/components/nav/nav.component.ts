701a4808045067e2384868448691c30e3fbd7270
argeulimbo <argeusousaptbr@gmail.com>
2025-06-03 13:42:44 -0300

    add: estilização do menu

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+// Angular Material
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
@@ -12,7 +15,8 @@ import { MatIconModule } from '@angular/material/icon';
     MatIconModule
   ],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class NavComponent {
 
