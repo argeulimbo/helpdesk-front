453f5a04dddd68a9d6c6efe9b6471c6f3cd2708d
argeulimbo <argeusousaptbr@gmail.com>
2025-06-04 13:38:41 -0300

    feat: import MatCardModule

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+// Angular Material
+import { MatCard, MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ MatCardModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
