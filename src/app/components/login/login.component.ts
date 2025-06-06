d020c4b59bb8a0c83ae6c4f56550bee50f0822ef
argeulimbo <argeusousaptbr@gmail.com>
2025-06-06 16:40:20 -0300

    feat: validação de campos de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,21 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ChangeDetectionStrategy } from '@angular/core';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { Credenciais } from '../../models/credenciais';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [ MatFormFieldModule,
              MatInputModule,
+             ReactiveFormsModule,
              FormsModule
    ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+
+export class LoginComponent implements OnInit {
+
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 }
