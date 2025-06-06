d905e7f987ad176e238440e7577e9bfed101ecf4
argeulimbo <argeusousaptbr@gmail.com>
2025-06-06 11:43:32 -0300

    style: estilização do login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ MatFormFieldModule,
+             MatInputModule,
+             FormsModule
+   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
