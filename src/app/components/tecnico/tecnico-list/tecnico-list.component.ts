e6a8e0b02ed99835a4a9fd81a0496fc9f5eeb452
argeulimbo <argeusousaptbr@gmail.com>
2025-06-26 16:03:59 -0300

    style: adicionado buttons em página de tecnico

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,44 +1,53 @@
 import { Component, ViewChild } from '@angular/core';
-
 import { RouterModule } from '@angular/router';
-
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatLabel, MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
 import { MatPaginator } from '@angular/material/paginator';
 import { Tecnico } from '../../../models/tecnico';
+import { TecnicoService } from '../../../services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-list',
   standalone: true,
   imports: [ RouterModule, 
              MatTableModule,
-             MatPaginator
+             MatPaginator,
+             MatLabel,
+             MatFormField,
+             MatFormFieldModule,
+             MatInputModule
   ],
   templateUrl: './tecnico-list.component.html',
   styleUrl: './tecnico-list.component.css'
 })
 export class TecnicoListComponent {
 
-  ELEMENT_DATA: Tecnico[] = [
-    { 
-      id: 1,
-      nome: 'Argeu Souza',
-      cpf: '822.432.998-10',
-      email: 'argeu@sonner.com.br',
-      senha: '1234',
-      perfis: ['0'],
-      dataCriacao: '15/08/2024'
-    }
-  ]
+  ELEMENT_DATA: Tecnico[] = []
 
   displayedColumns: string[] = ['idTecnico', 'nomeTecnico', 'cpfTecnico', 'emailTecnico', 'acoes'];
   dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(
+    private service: TecnicoService
+  ) { }
+
   ngOnInit(): void{
+    this.findAll();
   }  
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  findAll() {
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Tecnico>(resposta);
+      this.dataSource.paginator = this.paginator;
+    })
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
