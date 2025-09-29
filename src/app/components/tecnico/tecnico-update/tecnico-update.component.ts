a7854fa53f671f2c21eea321ba24a2683a36dd33
argeulimbo <argeusousaptbr@gmail.com>
2025-09-29 11:10:34 -0300

    feat: create component /tecnico/tecnico-update

---
+++ src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './tecnico-update.component.html',
+  styleUrl: './tecnico-update.component.css'
+})
+export class TecnicoUpdateComponent {
+
+}
