ca877ffad50be75b9b6a59dfe7f6f0a7f8e6ea0a
argeulimbo <argeusousaptbr@gmail.com>
2025-10-02 15:43:12 -0300

    feat: adjust style chamado-list

--- src/app/components/chamado/chamado-list/chamado-list.component.ts
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatLabel, MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatPaginator } from '@angular/material/paginator';
+import { Chamado } from '../../../models/chamado';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-chamado-list',
   standalone: true,
-  imports: [],
+  imports: [
+             RouterModule, 
+             MatTableModule,
+             MatPaginator,
+             MatLabel,
+             MatFormField,
+             MatFormFieldModule,
+             MatInputModule,
+             MatIconModule
+  ],
   templateUrl: './chamado-list.component.html',
   styleUrl: './chamado-list.component.css'
 })
 export class ChamadoListComponent {
+
+  ELEMENT_DATA: Chamado[] = []
+
+  displayedColumns: string[] = ['id', 'dataAbertura', 'dataFechamento', 'prioridade', 'status', 'titulo', 'descricao', 'tecnico', 'cliente', 'nomeCliente', 'nomeTecnico'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
