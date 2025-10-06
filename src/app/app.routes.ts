ac94e419d39ac1d8ae17a9188ffc5a86a75a01a7
argeulimbo <argeusousaptbr@gmail.com>
2025-10-06 16:05:19 -0300

    feat: component chamado-list created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecn
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -27,13 +28,13 @@ export const routes: Routes = [
             { path: 'tecnicos/create', component: TecnicoCreateComponent },
             { path: 'tecnicos/update/:id', component: TecnicoUpdateComponent },
             { path: 'tecnicos/delete/:id', component: TecnicoDeleteComponent },
-            { path: '', redirectTo: 'tecnicos', pathMatch: 'full' },
 
             { path: 'clientes', component: ClienteListComponent },
             { path: 'clientes/create', component: ClienteCreateComponent },
             { path: 'clientes/update/:id', component: ClienteUpdateComponent }, 
             { path: 'clientes/delete/:id', component: ClienteDeleteComponent },
-            { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+
+            { path: 'chamados', component: ChamadoListComponent} 
         ]
     }
 ]
