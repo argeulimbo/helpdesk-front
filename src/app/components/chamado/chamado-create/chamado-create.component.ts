60c6128aa48f2cdb7fb74ac311ea7725e5f94356
argeulimbo <argeusousaptbr@gmail.com>
2025-10-14 16:40:48 -0300

    feat: implemented chamado-create.ts

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,6 +9,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Cliente } from '../../../models/cliente';
+import { ClienteService } from '../../../services/cliente.service';
+import { Tecnico } from '../../../models/tecnico';
+import { TecnicoService } from '../../../services/tecnico.service';
+import { ChamadoService } from '../../../services/chamado.service';
+import { Chamado } from '../../../models/chamado';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-chamado-create',
@@ -28,24 +35,74 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css'
 })
-export class ChamadoCreateComponent {
+export class ChamadoCreateComponent implements OnInit {
 
-  prioridade: FormControl = new FormControl(null, [Validators.required]);
-  status:     FormControl = new FormControl(null, [Validators.required]);
-  titulo:     FormControl = new FormControl(null, [Validators.required]);
-  descricao:  FormControl = new FormControl(null, [Validators.required]);
-  tecnico:    FormControl = new FormControl(null, [Validators.required]);
-  cliente:    FormControl = new FormControl(null, [Validators.required]);
+  clientes: Cliente[] = [];
+  tecnicos: Tecnico[] = [];
 
-  constructor() { }
+  chamado: Chamado = {
+    prioridade:   '',
+    status:       '',
+    titulo:       '',
+    observacoes:  '',
+    tecnico:      '',
+    cliente:      '',
+    nomeCliente:  '',
+    nomeTecnico:  '',
+  }
+
+  prioridade:  FormControl = new FormControl(null, [Validators.required]);
+  status:      FormControl = new FormControl(null, [Validators.required]);
+  titulo:      FormControl = new FormControl(null, [Validators.required]);
+  observacoes: FormControl = new FormControl(null, [Validators.required]);
+  tecnico:     FormControl = new FormControl(null, [Validators.required]);
+  cliente:     FormControl = new FormControl(null, [Validators.required]);
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private clienteService: ClienteService,
+    private tecnicoService: TecnicoService,
+    private router: Router,
+    private toast: ToastrService
+  ) { }
+
+  ngOnInit(): void {
+    this.findAllClientes();
+    this.findAllTecnicos();
+  }
+  
+  cancelar() {
+    this.router.navigate(['/chamados']);
+  }
 
   validaCampos(): boolean {
     return this.prioridade.valid &&
            this.status.valid     &&
            this.titulo.valid     &&
-           this.descricao.valid  &&
+           this.observacoes.valid  &&
            this.tecnico.valid    &&
            this.cliente.valid;
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    });
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    });
+  }
+
+  create(): void {
+    this.chamadoService.create(this.chamado).subscribe( resposta => {
+      this.toast.success('Chamado criado com sucesso', 'Novo chamado');
+      this.router.navigate(['/chamados']);
+    }, ex => {
+      this.toast.error(ex.error);
+    })
   }
 
 }
