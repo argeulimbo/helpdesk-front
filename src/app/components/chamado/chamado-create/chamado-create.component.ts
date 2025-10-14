580b6cc78e762c05e990b401d4f1da3caf1fc68c
argeulimbo <argeusousaptbr@gmail.com>
2025-10-14 09:43:14 -0300

    feat: validation of forms to chamado-create

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chamado-create',
@@ -21,11 +22,30 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css'
 })
 export class ChamadoCreateComponent {
+
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status:     FormControl = new FormControl(null, [Validators.required]);
+  titulo:     FormControl = new FormControl(null, [Validators.required]);
+  descricao:  FormControl = new FormControl(null, [Validators.required]);
+  tecnico:    FormControl = new FormControl(null, [Validators.required]);
+  cliente:    FormControl = new FormControl(null, [Validators.required]);
+
+  constructor() { }
+
+  validaCampos(): boolean {
+    return this.prioridade.valid &&
+           this.status.valid     &&
+           this.titulo.valid     &&
+           this.descricao.valid  &&
+           this.tecnico.valid    &&
+           this.cliente.valid;
+  }
 
 }
